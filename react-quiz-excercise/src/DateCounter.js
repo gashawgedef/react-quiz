18c4c020164cf08d2ac5dd58d8168113f165f0f0
gashawgedef@gmail.com <gashawgedef@gmail.com>
2023-10-23 12:02:09 -0700

    gashit

--- react-quiz-excercise/src/DateCounter.js
+++ react-quiz-excercise/src/DateCounter.js
@@ -1,7 +1,14 @@
-import { useState } from "react";
+import { useReducer, useState } from "react";
 
+
+
+ function reducer(state,action){
+   return state+action;
+ }
 function DateCounter() {
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
+
+  const[count,dispatch]=useReducer(reducer,0);
   const [step, setStep] = useState(1);
 
   // This mutates the date object.
@@ -9,25 +16,29 @@ function DateCounter() {
   date.setDate(date.getDate() + count);
 
   const dec = function () {
+    dispatch(-1)
     // setCount((count) => count - 1);
-    setCount((count) => count - step);
+    // setCount((count) => count - step);
   };
 
   const inc = function () {
+    dispatch(1)
     // setCount((count) => count + 1);
-    setCount((count) => count + step);
+    // setCount((count) => count + step);
   };
 
   const defineCount = function (e) {
-    setCount(Number(e.target.value));
+    dispatch(Number(e.target.value))
+    // setCount(Number(e.target.value));
   };
 
   const defineStep = function (e) {
+   
     setStep(Number(e.target.value));
   };
 
   const reset = function () {
-    setCount(0);
+    // setCount(0);
     setStep(1);
   };
 
