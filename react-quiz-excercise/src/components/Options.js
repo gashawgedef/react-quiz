ba09fff07c49999f2a21df73bc1d3e98e9e96da4
gashawgedef@gmail.com <gashawgedef@gmail.com>
2023-10-24 09:12:18 -0700

    updates on the react quizzes

--- react-quiz-excercise/src/components/Options.js
+++ react-quiz-excercise/src/components/Options.js
@@ -1,15 +1,15 @@
 import React from 'react'
 
-function Options() {
-  return (
-    <div>
-         <div className='options'>
-        {question.options.map((option)=>(<button className='btn btn-option' key={option}>{option}</button>))}
+function Options({question}) {
+    return (
+        <div>
+            <div className='options'>
+                {question.options.map((option) => (<button className='btn btn-option' key={option}>{option}</button>))}
 
-      </div>
-      
-    </div>
-  )
+            </div>
+
+        </div>
+    )
 }
 
 export default Options
